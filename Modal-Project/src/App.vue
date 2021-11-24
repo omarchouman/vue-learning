c15673d6b31de92269b0c69fda4bc3b5d2f0eed8
omarchouman <omar.chouman0@gmail.com>
2021-11-25 00:16:22 +0200

    Finished Modal-Project

---
+++ Modal-Project/src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="firstname">
+  <button @click="handleClick">click here</button>
+  <div v-if="showModal">
+    <Modal header="Sign up for the GIVEAWAY!!" theme="sale" @close="toggleModal">
+      <!-- This Form Is A Slot -->
+      <form action="#">
+        <label for="name">First Name</label>
+        <input type="text" id="name">
+        <br/>
+        <label for="email">Email Address</label>
+        <input type="text" id="email">
+        <br/>
+        <button type="submit">Send</button>
+      </form>
+    </Modal>
+  </div>
+
+  <div v-if="showModal2">
+    <Modal header="Sign up for the GIVEAWAY!!" theme="sale" @close="toggleModal2">
+
+      <!-- This Table Is A Named Slot -->
+      <template v-slot:tablo> 
+        
+          <tr>
+            <th>Product</th>
+            <th>Price</th>
+          </tr>
+          <tr>
+            <td>Samsung</td>
+            <td>440$</td>
+          </tr>
+          <tr>
+            <td>Iphone</td>
+            <td>1500$</td>
+          </tr>
+      
+      </template>
+
+    </Modal>
+  </div>
+
+  <!-- First Modal Button -->
+  <button @click="toggleModal">open modal</button>
+  <!-- Second Modal Button -->
+  <button @click="toggleModal2">open modal 2</button>
+
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data(){
+    return {
+      title: "My First Vue App",
+      showModal: false,
+      showModal2: false
+    }
+  },
+  methods: {
+    handleClick() {
+      // Template Refs
+      this.$refs.firstname.classList.add('active');
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModal2() {
+      this.showModal2 = !this.showModal2
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ Modal-Project/src/components/Modal.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{ sale: theme === 'sale'}">
+            <h2>{{ header }}</h2>
+            <p>{{ text }}</p>
+
+            <!-- This Is How Slots Are Used -->
+            <slot></slot>
+
+            <!-- This Is How Named Slots Are Used -->
+            <table class="price__table">
+                <slot name="tablo"></slot>
+            </table>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: ['header', 'theme'],
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    },
+    data() {
+        return {
+            text: "modal content"
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+    .modal {
+        width:400px;
+        padding:20px;
+        margin: 200px auto;
+        background:white;
+        border-radius:10px;
+    }
+    .backdrop {
+        top:0;
+        position:fixed;
+        background:rgba(0, 0, 0, 0.5);
+        width:100%;
+        height:100%;
+    }
+    .modal.sale {
+        background: crimson;
+        color:white;
+    }
+    .modal.sale h2 {
+        color: white;
+    }
+    .price__table{
+      margin:0 auto;
+    }
+</style>
