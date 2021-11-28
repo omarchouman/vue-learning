a73f9fbdaa369fdf63b543c3f603ac090dd0ac07
omarchouman <omar.chouman0@gmail.com>
2021-11-28 18:25:05 +0200

    Finished Reaction-Timer Project

---
+++ Reaction-Timer/src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time - {{ score }} ms</p>
+  <p class="rank"> {{ rank }} </p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = "Fast Fingers Sir";
+        } else if (this. score < 400) {
+            this.rank = "Rapid Reflexes Sir";
+        } else {
+            this.rank = "Slow Reflexes Sir"
+        }
+    }
+}
+</script>
+
+<style>
+    .rank {
+        font-size:1.4em;
+        color:#0faf87;
+        font-weight:bold;
+    }
+</style>
